b6ac545049c058884ff221ddbf395621de99bf97
Erlandsson-Simon <erlandsson.simon@elev.ga.ntig.se>
2022-02-17 19:40:38 +0100

    :))

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-var suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
-var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-var deck = new Array();
+let suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
+let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+let deck = new Array();
 
-var dealerHand = new Array();
-var playerHand = new Array();
+let dealerHand = new Array();
+let playerHand = new Array();
 
-var playerTotalWeight;
+let playerTotalWeight;
 
-var deckBtn = document.querySelector(".newDeckBtn");
-deckBtn.addEventListener("click", createDeck);
+let num = 4;
+
+let dealerLow = true;
+
+let gameOver = false;
+let gameDraw = false;
+let gameWon = false;
+
+let playerCardAmount = document.querySelector(".playerCards");
+let dealerCardAmount = document.querySelector(".dealerCards");
+
+
+let dealButton = document.querySelector(".dealCardsButton");
+dealButton.addEventListener("click", createDeck);
+
+let hitButton = document.querySelector(".hitButton");
+hitButton.addEventListener("click", hit);
+
+let stayButton = document.querySelector(".stayButton");
+stayButton.addEventListener("click", stay);
 
 function createDeck() {
-    deck = new Array();
+    deck = [];
     values.forEach(values => {
         suits.forEach(suits => {
-            var weight = parseInt(values);
+            let weight = parseInt(values);
             if(values == "J" || values == "Q" || values == "K")
             {
                 weight = 10;
@@ -23,32 +41,96 @@ function createDeck() {
             {
                 weight = 11;
             }
-            var card = {Value: values, Suit: suits, Weight: weight}
+            let card = {Value: values, Suit: suits, Weight: weight}
             deck.push(card);
         });
     });
     shuffleDeck(deck);
-    console.log("test");
+    dealCards();
 }
 
 function shuffleDeck(deck) {
-    var sort = deck.sort((a, b) => 0.5 - Math.random());
+    let sort = deck.sort((a, b) => 0.5 - Math.random());
     return sort;
 }
 
 function dealCards() {
-    dealerHand.push(deck[0], deck[3]);
-    playerHand.push(deck[1], deck[2]);
+    dealerHand.push(deck[0]);
+    deck.shift();
+    playerHand.push(deck[0]);
+    deck.shift();
+    playerHand.push(deck[0]);
+    deck.shift();
+    dealerHand.push(deck[0]);
+    deck.shift();
+
+    playerCardAmount.innerHTML = "Player cards: " + checkPlayerWeight();
+    dealerCardAmount.innerHTML = "Dealer cards: " + dealerHand[0].Weight;
 }
 
 function checkPlayerWeight() {
-    playerTotalWeight = 0;
-    parseInt(playerTotalWeight);
+    let cardWeight = 0;
     playerHand.forEach(playerHand => {
-        playerTotalWeight += playerHand.weight;
+        cardWeight += playerHand.Weight;
     });
+    return cardWeight;
 }
 
-function startGame() {
+function checkDealerWeight() {
+    let dealerCardWeight = 0;
+    dealerHand.forEach(dealerHand => {
+        dealerCardWeight += dealerHand.Weight;
+    })
+    return dealerCardWeight;
+}
+
+function hit() {
+    playerHand.push(deck[0])
+    deck.shift();
+    if(checkPlayerWeight() > 21) {
+        playerHand.forEach(playerHand => {
+            if(playerHand.Value == "A") {
+                playerHand.Weight = 1
+            }
+        }) 
+        if(checkPlayerWeight() > 21) {
+            console.log("You have lost");
+        }
+    }
+    playerCardAmount.innerHTML = "Player cards: " + checkPlayerWeight();
+}
+
+function dealerHit() {
+    dealerHand.push(deck[0]);
+    deck.shift();
+}
+
+function stay() {
+
+    if(checkDealerWeight > 16) {
+        dealerLow = false;
+    }
     
+    while(dealerLow) {
+        dealerHit();
+        dealerCardAmount.innerHTML = "Dealer cards: " + checkDealerWeight();
+
+        if(checkDealerWeight() > 16) {
+            dealerLow = false;
+        }
+        
+    }
+    
+    if (checkDealerWeight() > 16) {
+        if(checkDealerWeight() > checkPlayerWeight() && checkDealerWeight() < 22) {
+            console.log("You have Lost...");
+            gameOver = true;
+        } else if (checkPlayerWeight() == checkDealerWeight()) {
+            console.log("Its a Draw.");
+            gameDraw = true;
+        } else {
+            console.log("You have Won!");
+            gameWon = true;
+        }
+    }
 }
