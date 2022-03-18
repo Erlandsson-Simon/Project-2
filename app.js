195a11d5873281982403bd72ed97ce3a74f6fc0b
Erlandsson-Simon <erlandsson.simon@elev.ga.ntig.se>
2022-03-18 12:28:10 +0100

    ye

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ let gameWon = false;
 let playerCardAmount = document.querySelector(".playerCards");
 let dealerCardAmount = document.querySelector(".dealerCards");
 
-
 let dealButton = document.querySelector(".dealCardsButton");
 dealButton.addEventListener("click", createDeck);
 
@@ -29,169 +28,207 @@ let standButton = document.querySelector(".standButton");
 standButton.addEventListener("click", stand);
 
 function createDeck() {
-    // 10 + ny deck
-    if(deck.length < 10) {
+  // 10 + ny deck
+  gameDraw = false;
+  gameEnd = false;
+  gameWon = false;
+
+  if (deck.length < 10) {
     deck = [];
-    values.forEach(values => {
-        suits.forEach(suits => {
-            let weight = parseInt(values);
-            if(values == "J" || values == "Q" || values == "K")
-            {
-                weight = 10;
-            }
-            if(values == "A") 
-            {
-                weight = 11;
-            }
-            let card = {Value: values, Suit: suits, Weight: weight}
-            deck.push(card);
-        });
+    values.forEach((values) => {
+      suits.forEach((suits) => {
+        let weight = parseInt(values);
+        if (values == "J" || values == "Q" || values == "K") {
+          weight = 10;
+        }
+        if (values == "A") {
+          weight = 11;
+        }
+        let card = { Value: values, Suit: suits, Weight: weight };
+        deck.push(card);
+      });
     });
-    
-}
-    shuffleDeck(deck);
-    dealCards();
+  }
+  shuffleDeck(deck);
+  dealCards();
 }
 
 function shuffleDeck(deck) {
-    let sort = deck.sort((a, b) => 0.5 - Math.random());
-    return sort;
+  let sort = deck.sort((a, b) => 0.5 - Math.random());
+  return sort;
 }
 
 function dealCards() {
-    dealerHand = []
-    playerHand = []
+  document.querySelector(".endScreen").style.display = "none";
 
-    dealerLow = true;
+  dealerHand = [];
+  playerHand = [];
 
-    dealerHand.push(deck[0]);
-    deck.shift();
+  dealerLow = true;
 
-    document.querySelector(".dealerCards").innerHTML = "";
-    document.querySelector(".dealerCards").append(dealerImgSelector());
-    
-    playerHand.push(deck[0]);
-    deck.shift();
+  dealerHand.push(deck[0]);
+  deck.shift();
 
-    playerHand.push(deck[0]);
-    deck.shift();
+  document.querySelector(".dealerCards").innerHTML = "";
+  document.querySelector(".dealerCards").append(dealerImgSelector());
 
-    dealerHand.push(deck[0]);
-    deck.shift();
+  playerHand.push(deck[0]);
+  deck.shift();
 
-    document.querySelector(".playerCards").innerHTML = "";
-    document.querySelector(".playerCards").append(playerImgSelector());
+  playerHand.push(deck[0]);
+  deck.shift();
 
-    playerCardText.innerHTML = "Player cards: 5"
-    dealerCardText.innerHTML = "Dealer cards: " + dealerHand[0].Weight;
+  dealerHand.push(deck[0]);
+  deck.shift();
+
+  document.querySelector(".playerCards").innerHTML = "";
+  document.querySelector(".playerCards").append(playerImgSelector());
+
+  document.querySelector(".playerCardText").innerHTML =
+    "Player cards: " + checkPlayerWeight();
+  document.querySelector(".dealerCardText").innerHTML =
+    "Dealer cards: " + dealerHand[0].Weight;
+
+  if (checkPlayerWeight() == 21) {
+    console.log("You have won");
+    gameWon = true;
+    gameEnd();
+  }
 }
 
 function checkPlayerWeight() {
-    let cardWeight = 0;
-    playerHand.forEach(playerHand => {
-        cardWeight += playerHand.Weight;
-    });
-    return cardWeight;
+  let cardWeight = 0;
+  playerHand.forEach((playerHand) => {
+    cardWeight += playerHand.Weight;
+  });
+  return cardWeight;
 }
 
 function checkDealerWeight() {
-    let dealerCardWeight = 0;
-    dealerHand.forEach(dealerHand => {
-        dealerCardWeight += dealerHand.Weight;
-    })
-    return dealerCardWeight;
+  let dealerCardWeight = 0;
+  dealerHand.forEach((dealerHand) => {
+    dealerCardWeight += dealerHand.Weight;
+  });
+  return dealerCardWeight;
 }
 
 function hit() {
-    playerHand.push(deck[0])
-    deck.shift();
-    document.querySelector(".playerCards").innerHTML = "";
-    document.querySelector(".playerCards").append(playerImgSelector());
-    if(checkPlayerWeight() > 21) {
-        playerHand.forEach(playerHand => {
-            if(playerHand.Value == "A") {
-                playerHand.Weight = 1
-            }
-        }) 
-        if(checkPlayerWeight() > 21) {
-            console.log("You have lost");
-        }
+  playerHand.push(deck[0]);
+  deck.shift();
+  document.querySelector(".playerCards").innerHTML = "";
+  document.querySelector(".playerCards").append(playerImgSelector());
+  if (checkPlayerWeight() > 21) {
+    playerHand.forEach((playerHand) => {
+      if (playerHand.Value == "A") {
+        playerHand.Weight = 1;
+      }
+    });
+    if (checkPlayerWeight() > 21) {
+      console.log("You have lo");
+      gameOver = true;
+      gameEnd();
     }
-   // playerCardAmount.innerHTML = "Player cards: " + checkPlayerWeight();
+  }
+  document.querySelector(".playerCardText").innerHTML =
+    "Player cards: " + checkPlayerWeight();
+
+  if (checkPlayerWeight() > 21) {
+    console.log("You have Lost...");
+    gameOver = true;
+    gameEnd();
+  }
 }
 
 function dealerHit() {
-    dealerHand.push(deck[0]);
-    deck.shift();
-    document.querySelector(".dealerCards").innerHTML = "";
-    document.querySelector(".dealerCards").append(dealerImgSelector());
+  dealerHand.push(deck[0]);
+  deck.shift();
+  document.querySelector(".dealerCards").innerHTML = "";
+  document.querySelector(".dealerCards").append(dealerImgSelector());
 }
 
 function stand() {
+  if (checkDealerWeight > 16) {
+    dealerLow = false;
+  }
 
-    if(checkDealerWeight > 16) {
-        dealerLow = false;
-    }
-    
-    while(dealerLow) {
-        dealerHit();
-       // dealerCardAmount.innerHTML = "Dealer cards: " + checkDealerWeight();
+  while (dealerLow) {
+    dealerHit();
 
-        if(checkDealerWeight() > 16) {
-            dealerLow = false;
-        }
-        
-    }
-    
+    document.querySelector(".dealerCardText").innerHTML =
+      "Dealer cards: " + checkDealerWeight();
+
     if (checkDealerWeight() > 16) {
-        if(checkDealerWeight() > checkPlayerWeight() && checkDealerWeight() < 22) {
-            console.log("You have Lost...");
-            gameOver = true;
-        } else if (checkPlayerWeight() == checkDealerWeight()) {
-            console.log("Its a Draw.");
-            gameDraw = true;
-        } else {
-            console.log("You have Won!");
-            gameWon = true;
-        }
+      dealerLow = false;
     }
+  }
+
+  if (checkDealerWeight() > checkPlayerWeight() && checkDealerWeight() < 22) {
+    console.log("You have Lost...");
+    gameOver = true;
+    gameEnd();
+  } else if (checkPlayerWeight() == checkDealerWeight()) {
+    console.log("Its a Draw.");
+    gameDraw = true;
+    gameEnd();
+  } else {
+    console.log("You have Won!");
+    gameWon = true;
+    gameEnd();
+  }
 }
 
-
 function playerImgSelector() {
-    let playerCardDiv = document.createElement("div");
+  let playerCardDiv = document.createElement("div");
 
-    playerHand.forEach(element => {
-        let playerCardImgSrc = "./CardImages/";
-        playerCardImgSrc += element.Suit;
-        playerCardImgSrc += "/";
-        playerCardImgSrc += element.Value;
-        playerCardImgSrc += ".png";
-        console.log(playerCardImgSrc);
-        let playerCardImg = document.createElement("img");
-        playerCardImg.setAttribute("src", playerCardImgSrc);
-        playerCardImg.setAttribute("alt", element.Value + "of" + element.Suit);
-        playerCardDiv.append(playerCardImg);    
-    });
+  playerHand.forEach((element) => {
+    let playerCardImgSrc = "./CardImages/";
+    playerCardImgSrc += element.Suit;
+    playerCardImgSrc += "/";
+    playerCardImgSrc += element.Value;
+    playerCardImgSrc += ".png";
+    console.log(playerCardImgSrc);
+    let playerCardImg = document.createElement("img");
+    playerCardImg.setAttribute("src", playerCardImgSrc);
+    playerCardImg.setAttribute("alt", element.Value + "of" + element.Suit);
+    playerCardDiv.append(playerCardImg);
+  });
 
-    return playerCardDiv;
+  return playerCardDiv;
 }
 
 function dealerImgSelector() {
-    let dealerCardDiv = document.createElement("div");
+  let dealerCardDiv = document.createElement("div");
 
-    dealerHand.forEach(element => {
-        let dealerCardImgSrc = "./CardImages/";
-        dealerCardImgSrc += element.Suit;
-        dealerCardImgSrc += "/";
-        dealerCardImgSrc += element.Value;
-        dealerCardImgSrc += ".png";
-        console.log(dealerCardImgSrc);
-        let dealerCardImg = document.createElement("img");
-        dealerCardImg.setAttribute("src", dealerCardImgSrc);
-        dealerCardImg.setAttribute("alt", element.Value + "of" + element.Suit);
-        dealerCardDiv.append(dealerCardImg);    
-    });
+  dealerHand.forEach((element) => {
+    let dealerCardImgSrc = "./CardImages/";
+    dealerCardImgSrc += element.Suit;
+    dealerCardImgSrc += "/";
+    dealerCardImgSrc += element.Value;
+    dealerCardImgSrc += ".png";
+    console.log(dealerCardImgSrc);
+    let dealerCardImg = document.createElement("img");
+    dealerCardImg.setAttribute("src", dealerCardImgSrc);
+    dealerCardImg.setAttribute("alt", element.Value + "of" + element.Suit);
+    dealerCardDiv.append(dealerCardImg);
+  });
 
-    return dealerCardDiv;
+  return dealerCardDiv;
+}
+
+function gameEnd() {
+  console.log("hello");
+  if (gameOver) {
+    document.querySelector(".endScreen").style.display = "flex";
+    document.querySelector(".endText").innerHTML = "You have lost!";
+  }
+
+  if (gameDraw) {
+    document.querySelector(".endScreen").style.display = "flex";
+    document.querySelector(".endText").innerHTML = "It's a draw!";
+  }
+
+  if (gameWon) {
+    document.querySelector(".endScreen").style.display = "flex";
+    document.querySelector(".endText").innerHTML = "You have won!";
+  }
 }
